3f00972a58e60b4cff087c9d173b8604f7f16b93
Maria Salas <masalaslinan05@gmail.com>
2026-06-12 19:53:55 -0500

    Inicialización del proyecto Spring Boot

---
+++ src/test/java/com/accesscontrol/access_control/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesscontrol.access_control.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
